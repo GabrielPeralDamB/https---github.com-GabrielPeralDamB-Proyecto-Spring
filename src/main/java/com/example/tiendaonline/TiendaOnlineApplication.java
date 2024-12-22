2e63492c7d1a45a58782b49b00f4bf7d9ccd5bfa
Gabriel <gabriel.peral@iesdoctorbalmis.com>
2024-12-22 17:48:59 +0100

    Lista usuarios

--- src/main/java/com/example/tiendaonline/TiendaOnlineApplication.java
+++ src/main/java/com/example/tiendaonline/TiendaOnlineApplication.java
@@ -2,12 +2,25 @@ package com.example.tiendaonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import com.example.tiendaonline.entidades.Usuario;
+import com.example.tiendaonline.servicios.UsuarioServicio;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class TiendaOnlineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaOnlineApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(UsuarioServicio usuarioServicio) {
+		return (args) -> {
+			usuarioServicio.add(new Usuario("Luis", "Jimenez", "luis@example.com", "admin", "12345678A", "Calle Falsa 123", "123456789", LocalDate.of(1990, 1, 1), "password", true));
+			usuarioServicio.add(new Usuario("Ana", "Perez", "ana@example.com", "user", "87654321B", "Avenida Siempre Viva 742", "987654321", LocalDate.of(1992, 2, 2), "password", true));
+		};
 	}
 
 }
--- src/main/java/com/example/tiendaonline/controladores/UsuarioController.java
+++ src/main/java/com/example/tiendaonline/controladores/UsuarioController.java
@@ -1,5 +1,19 @@
 package com.example.tiendaonline.controladores;
 
-public class UsuarioController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import com.example.tiendaonline.servicios.UsuarioServicio;
 
+@Controller
+public class UsuarioController {
+    @Autowired
+    private UsuarioServicio usuarioServicio;
+
+    @GetMapping("/usuarios")
+    public String listUsuarios(Model model) {
+        model.addAttribute("usuarios", usuarioServicio.findAll());
+        return "listausuario";
+    }
 }
