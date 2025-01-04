ca60055d549e8d06ce2bcdcbbabcc568cfdfd015
Gabriel <gabriel.peral@iesdoctorbalmis.com>
2025-01-04 15:41:23 +0100

    añadido 3 ejemplso de usuarios base

--- src/main/java/com/example/tiendaonline/TiendaOnlineApplication.java
+++ src/main/java/com/example/tiendaonline/TiendaOnlineApplication.java
@@ -20,6 +20,9 @@ public class TiendaOnlineApplication {
 		return (args) -> {
 			usuarioServicio.add(new Usuario("Luis", "Jimenez", "luis@example.com", "admin", "12345678A", "Calle Falsa 123", "123456789", LocalDate.of(1990, 1, 1), "password", true));
 			usuarioServicio.add(new Usuario("Ana", "Perez", "ana@example.com", "user", "000", "Avenida Siempre Viva 742", "987654321", LocalDate.of(1992, 2, 2), "password", true));
+			usuarioServicio.add(new Usuario("usuario", "usuario", "usuario@usuario.com", "usuario", "00000000P", "usuario street", "00000000", LocalDate.of(1990, 1, 1), "usuario", true));
+			usuarioServicio.add(new Usuario("admin", "admin", "admin@admin.com", "admin", "11111111P", "admin street", "111111111", LocalDate.of(1990, 1, 1), "admin", true));
+			usuarioServicio.add(new Usuario("empleado", "empleado", "empleado@empleado.com", "empleado", "22222222P", "empleado street", "222222222", LocalDate.of(1992, 2, 2), "empleado", true));
 		};
 	}
 
